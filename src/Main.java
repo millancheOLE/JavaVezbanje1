34bbe24daa823b96230e0eb36b919e76257c0ce0
millancheOLE <millanche_OLE@hotmail.com>
2022-09-24 18:41:29 +0200

    Java Practice no.1 - working with multiple classes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+/* Napraviti klasu koja predstavlja predracun. Treba da ima ime proizvoda i cenu (po komadu).
+U main metodi napraviti nekoliko predracuna unoseci vrednosti iskljucivo kroz konstruktor.
+Napisati metodu u klasi  za predracun koja ce da izracuna koliko kosta proizvod za uneti kvantitet (preko parametra proslediti).
+U main metodi ispisati koliko koji proizvod kosta za taj neki kvantitet.
+ */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        String productName;
+        int productPrice;
+        int quantity;
+
+        System.out.print("Unesite ime proizvoda: ");
+        productName = scan.nextLine();
+
+        System.out.print("Unesite cenu proizvoda po komadu: ");
+        productPrice = scan.nextInt();
+
+        ProInvoice product = new ProInvoice(productName, productPrice);
+
+        System.out.println("Cena za " + productName.toLowerCase() + " po komadu je " + productPrice + " rsd.");
+
+        System.out.print("Unesite koliko komada zelite da kupite: ");
+        quantity = scan.nextInt();
+
+        System.out.println("Cena za " + quantity + " komada je " + product.quantity(quantity) + " rsd.");
     }
 }
--- src/ProInvoice.java
+++ src/ProInvoice.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ProInvoice {
+/* Napraviti klasu koja predstavlja predracun. Treba da ima ime proizvoda i cenu (po komadu).
+U main metodi napraviti nekoliko predracuna unoseci vrednosti iskljucivo kroz konstruktor.
+Napisati metodu u klasi  za predracun koja ce da izracuna koliko kosta proizvod za uneti kvantitet (preko parametra proslediti).
+U main metodi ispisati koliko koji proizvod kosta za taj neki kvantitet.
+ */
+
+public class ProInvoice {
+
+    String productName;
+    int productPrice;
+
+    public ProInvoice(String name, int productPrice) {
+        this.productName = name;
+        this.productPrice = productPrice;
+    }
+
+    public int quantity(int quantity) {
+
+        int total;
+        total = quantity * this.productPrice;
+
+        return total;
+    }
 }
